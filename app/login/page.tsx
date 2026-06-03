0ca3c0565ac09f51f74782fc3b65278c63572a17
Ap17-dl <ankush170306@gmail.com>
2026-06-03 18:56:25 +0530

    Made changes to onboarding page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -146,7 +146,7 @@ export default function LoginPage() {
           </Card>
 
           <p className="text-center text-sm text-muted-foreground">
-            Don&apos;t have an account?{' '}
+            Don't have an account?{' '}
             <Link href="/signup" className="text-primary hover:underline font-medium">
               Sign up
             </Link>
